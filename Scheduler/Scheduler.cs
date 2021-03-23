4c286bcc8f224e5f907972596bed7732a2c102cc
Vimal CK <vimal-ck@outlook.com>
2021-03-23 20:24:26 +0000

    Brought back Scheduler

---
+++ Scheduler/Scheduler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Scheduler
+{
+    internal class Scheduler : ListBox
+    {
+        public Scheduler()
+        {
+            this.DefaultStyleKey = typeof(ListBox);
+        }
+    }
+}
