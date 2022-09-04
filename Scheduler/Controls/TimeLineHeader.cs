f114ce2ddc2e35cf93b63fb9b72e79ef38076380
Vimal CK <vimal-ck@outlook.com>
2022-09-04 01:06:07 +0100

    Timeline drawing base implementation

--- Scheduler/Controls/TimeLineHeader.cs
+++ Scheduler/Controls/TimeLineHeader.cs
@@ -1,24 +1,20 @@
-﻿using Scheduler.Common;
-using System;
-using System.Globalization;
+﻿using System;
+using System.Security.Cryptography;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media;
-using static Scheduler.UIExtensions;
+using Scheduler.Common;
 using static Scheduler.Common.Values;
 
 namespace Scheduler
 {
     internal sealed class TimeLineHeader : FrameworkElement
     {
+        private double widthOfHour;
+        private double averageHeight;
         private ScheduleControl parent;
-        //private TranslateTransform transform = new();
         private DrawingGroup backingStore = new();
-        public TimeLineHeader()
-        {
-            DefaultStyleKey = typeof(TimeLineHeader);
-            //this.RenderTransform = transform;
-        }
+
+        public TimeLineHeader() => DefaultStyleKey = typeof(TimeLineHeader);
 
         ~TimeLineHeader() => parent.ScrollChanged -= ParentScrollChanged;
 
@@ -31,39 +27,42 @@ namespace Scheduler
 
         private void ParentScrollChanged(object sender, System.Windows.Controls.ScrollChangedEventArgs e)
         {
-            if (e.HorizontalChange != Zero)
+            if (e.HorizontalChange is not Zero)
             {
-                return; //transform.X -= e.HorizontalChange;
+                RenderContent(e.HorizontalOffset);
             }
-
-            RenderContent(e.HorizontalChange);
         }
 
         protected override void OnRender(DrawingContext drawingContext)
         {
-            if (parent is null || ActualWidth == Zero)
+            if (parent is null || ActualWidth is Zero)
             {
                 return;
             }
 
+            widthOfHour = parent.TestSize.Width / 24;
+            averageHeight = ActualHeight / 3;
             drawingContext.DrawBorder(this, parent.TimeLineColor, BorderThickness);
             drawingContext.DrawDrawing(backingStore);
 
             RenderContent();
-
         }
 
-        private void RenderContent(double offset = 0)
+        private void RenderContent(double horizontalOffset = 0)
         {
-            var lineStartPoint = new Point();
-            var lineEndPoint = new Point(0, ActualHeight);
+            var hoursScrolled = horizontalOffset / widthOfHour;
+            var remainingWidthToScroll = (((int)hoursScrolled + 1) * widthOfHour) - horizontalOffset;
+            var lineStartPoint = new Point(remainingWidthToScroll, 0);
+            var lineEndPoint = new Point(remainingWidthToScroll, ActualHeight);
             var drawingContext = backingStore.Open();
 
-            DrawTimeline(drawingContext, ref lineStartPoint, ref lineEndPoint);
+            DrawTimeline(drawingContext, ref lineStartPoint, ref lineEndPoint, (int)hoursScrolled);
             drawingContext.DrawLine(new Pen(parent.TimeLineColor, HeaderLineThickness), lineStartPoint, lineEndPoint);
 
             if (parent.TimeLineZoom.Equals(TimeLineZoom.FortyEight))
             {
+                lineStartPoint.X += widthOfHour;
+                lineEndPoint.X += widthOfHour;
                 DrawTimeline(drawingContext, ref lineStartPoint, ref lineEndPoint);
                 drawingContext.DrawLine(new Pen(parent.TimeLineColor, HeaderLineThickness), lineStartPoint, lineEndPoint);
             }
@@ -71,24 +70,23 @@ namespace Scheduler
             drawingContext.Close();
         }
 
-        private void DrawTimeline(DrawingContext drawingContext, ref Point lineStartPoint, ref Point lineEndPoint)
+        private void DrawTimeline(DrawingContext drawingContext,
+            ref Point lineStartPoint,
+            ref Point lineEndPoint,
+            int startFrom = default)
         {
-            var headerText = Zero;
-            var averageHeight = ActualHeight / 3;
-            var timeLineGap = parent.TestSize.Width / 24;
-            var clipWidth = Math.Max(0, lineStartPoint.X + timeLineGap - 3);
-
-            for (int timeColumn = 0; timeColumn < 24; timeColumn++)
+            //var clipWidth = Math.Max(0, lineStartPoint.X + timeLineGap - 3);
+            for (int timeColumn = startFrom; timeColumn < 23; timeColumn++)
             {
-                var formattedTime = TimeSpan.FromHours(headerText++).ToString(TimeFormat);
+                var formattedTime = TimeSpan.FromHours(timeColumn).ToString(TimeFormat);
 
-                drawingContext.PushClip(clipWidth, ActualHeight);
-                drawingContext.DrawText(this, formattedTime, lineStartPoint.X + TimeHeaderOffset, averageHeight);
+                //drawingContext.PushClip(clipWidth, ActualHeight);
+                // drawingContext.DrawText(this, formattedTime, lineStartPoint.X + TimeHeaderOffset, averageHeight);
                 drawingContext.DrawLine(new Pen(parent.TimeLineColor, NarrowThickness), lineStartPoint, lineEndPoint);
-                drawingContext.Pop();
-                lineStartPoint.X += timeLineGap;
+                //drawingContext.Pop();
+                lineStartPoint.X += widthOfHour;
                 lineEndPoint.X = lineStartPoint.X;
-                clipWidth += timeLineGap;
+                //clipWidth += timeLineGap;
             }
         }
     }
